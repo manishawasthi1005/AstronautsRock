8f43fe571a5a6d35d27ba0eb93c034cb0a9115a1
m.a.awasthi <Manish.Awasthi@externals.adidas.com>
2023-05-06 12:04:10 +0200

    Initial Commit

---
+++ AstronautsRock/AstronautsRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronautsRockApp.swift
+//  AstronautsRock
+//
+//  Created by Manish Awasthi on 06.05.23.
+//
+
+import SwiftUI
+
+@main
+struct AstronautsRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
