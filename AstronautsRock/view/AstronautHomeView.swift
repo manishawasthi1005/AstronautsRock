f6ede0634c4b99db816188f0219f798c21cfeaaf
m.a.awasthi <Manish.Awasthi@externals.adidas.com>
2023-05-08 13:54:34 +0200

    Committing FaceId related changed

--- AstronautsRock/view/AstronautHomeView.swift
+++ AstronautsRock/view/AstronautHomeView.swift
@@ -5,28 +5,47 @@
 //  Created by Manish Awasthi on 05.05.23.
 //
 import SwiftUI
+import LocalAuthentication
 
 struct AstronautHomeView: View {
     @EnvironmentObject var astronautRestData: AstronautsRestData
+    @State private var unlocked = false
 
     var body: some View {
-        
         NavigationView {
-        
-            List(astronautRestData.astronauts) { astronaut in
-                        NavigationLink {
-                            AstronautDetailView(astronaut: astronaut)
-                        } label: {
-                            AstronautRowView(astronaut: astronaut)
-                        }
-            }
-            .navigationTitle("Astronauts")
-            .listStyle(.grouped)
+            if unlocked{
+                List(astronautRestData.astronauts) { astronaut in
+                    NavigationLink {
+                        AstronautDetailView(astronaut: astronaut)
+                    } label: {
+                        AstronautRowView(astronaut: astronaut)
+                    }
                 }
-        .onAppear {
-            astronautRestData.getAstronauts()
+                .navigationTitle("Astronauts")
+                .listStyle(.grouped)
+            }
         }
-      
+        .onAppear {
+            authenticate()
+            astronautRestData.getAstronauts()
+    }
+    }
+    func authenticate(){
+        let context = LAContext()
+        var error: NSError?
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "This is for security reasons"){
+                success, authenticationError in
+                if success{
+                    unlocked = true
+                }else{
+                    //There was a problem
+                }
+            }
+            
+        }else{
+            //Phone does not have Biometrics
+        }
     }
 }
 
